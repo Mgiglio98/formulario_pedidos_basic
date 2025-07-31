badd13d44c17b90838921ea7b1d362325386d14e
Mgiglio98 <matheusgiglio6@gmail.com>
2025-07-31 15:35:39 -0300

    Update formulario.py

--- formulario.py
+++ formulario.py
@@ -205,11 +205,11 @@ with st.expander("➕ Adicionar Insumo", expanded=True):
         if descricao_final and quantidade > 0 and (usando_base or unidade.strip()):
             if usando_base:
                 linha_insumo = df_insumos[df_insumos["Descrição"] == descricao_final].iloc[0]
-                min_qtd = linha_insumo.get("Min")
+                #min_qtd = linha_insumo.get("Min")
 
-                if pd.notna(min_qtd) and quantidade < min_qtd:
-                    st.error(f"⚠️ Quantidade mínima para esse item é {int(min_qtd)}. Solicite no mínimo esse valor.")
-                    st.stop()
+                #if pd.notna(min_qtd) and quantidade < min_qtd:
+                    #st.error(f"⚠️ Quantidade mínima para esse item é {int(min_qtd)}. Solicite no mínimo esse valor.")
+                    #st.stop()
                 
             novo_insumo = {
                 "descricao": descricao_final,
