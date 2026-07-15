0301702d86fb4432618c3f9339832f17be5645e9
Mgiglio98 <matheusgiglio6@gmail.com>
2026-07-15 10:52:38 -0300

    Update formulario.py

--- formulario.py
+++ formulario.py
@@ -169,7 +169,7 @@ OBRA_EXECUTIVOS = {
     "2605 - ALESSANDRO ARDUINI": [
         {"executivo": "Carolina Oliveira", "email": "carolina.oliveira@osborne.com.br"},
     ],
-    "2606 - MARCO FREIRE (FAZENDA BOA VISTA)": [
+    "2606 - MARCO AURÉLIO SIMÃO FREIRE": [
         {"executivo": "Vitor Carvalho", "email": "vitor.carvalho@osborne.com.br"},
         {"executivo": "Caio Fausto", "email": "caio.fausto@osborne.com.br"},
     ],
